c5e494e158030148b5c92f7156cc349f612efc12
amaryange <melessangekevin@gmail.com>
2024-04-06 23:39:38 +0000

    initial commit

---
+++ src/main/java/dev/amarycode/testcicd/controller/SAController.java
@@ -0,0 +1,19 @@
+package dev.amarycode.testcicd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class SAController {
+
+    @GetMapping("/")
+    public Map<String, Object> index(){
+        Map<String, Object> map = new HashMap<>();
+        map.put("message", "Hello World!");
+        return map;
+    }
+
+}
